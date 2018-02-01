5f5ca2a359aa961070b7456544be64e3fb38a860
Kevin L. Rak <kevinlrak@users.noreply.github.com>
2018-02-01 11:38:14 -0500

    Download multiple images

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+//Forked from https://github.com/nichlock/NASA_Desktop_Image-of-the-Day
 //This Java file gets the location of the NASA image of the day and downloads it to a new file either labeled img.jpg or img2.jpg
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,14 +15,17 @@ import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String args[]) { // This is the first section of code to run
-    	saveImage(parseXml()); // Set the image to the image pulled from NASA's RSS feed
+    public static void main(String args[])
+    {
+        for (int i = 0; i < 10; i++)   // typically between 10 - 15 images can be downloaded
+    	    saveImage(parseXml(i), i); // Save the image pulled from NASA's RSS feed
     }
     
-    private static String parseXml() // The second class to run, returns the image link pulled from the RSS feed
-    {								 // This class was 
+    private static String parseXml(int i) // Returns the image link pulled from the RSS feed
+    {
     	String imagelink = "";
         try 
         {
@@ -31,41 +35,34 @@ public class Main {
             Document doc=dBuilder.parse(conn.getInputStream());
             doc.getDocumentElement().normalize();
             NodeList nList=doc.getElementsByTagName("item");
-            Node nNode=nList.item(0);
+            Node nNode=nList.item(i);
           
-
-                    if(nNode.getNodeType()==Node.ELEMENT_NODE)
-                    {
-                        Element eElement=(Element) nNode;
-                        String temp=eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("url")+"";
-                        imagelink=temp.replace("url=\"", "").replace("\"", "").trim();
-                    }
-                    return imagelink;
-            
-            
-        } catch (Exception ex) { //If the RSS can't be accessed then open a window to warn the user
+            if(nNode.getNodeType() == Node.ELEMENT_NODE)
+            {
+                Element eElement=(Element) nNode;
+                String temp=eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("url")+"";
+                imagelink=temp.replace("url=\"", "").replace("\"", "").trim();
+            }
+            return imagelink;
+        } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Error while getting RSS.\nPerhaps try checking your internet connection?");
             return null;
         }
-        
     }
-    public static void saveImage(String imageURL) // The third class to run, this saves the image to the specified location
+    
+    public static void saveImage(String imageURL, int i) // save the image to the specified location
     {
     	// For troubleshooting:
         System.out.println(imageURL.substring(0,4) + "s" + imageURL.substring(4));
-        try { //try the following code:
-        	
+        try {
             // Get the image from the URL, and set it to the image files
             URL url = new URL(imageURL.substring(0,4) + "s" + imageURL.substring(4));
             BufferedImage imageS = null;
             imageS = ImageIO.read(url);
-            ImageIO.write(imageS, "jpg", new File("img.jpg")); //writes to two image files, this allows for Windows 7 compatibility
-            ImageIO.write(imageS, "jpg", new File("img2.jpg"));
-            
-        } catch (Exception ex) { // If the previous code failed for some reason, then open a windows that states the failure
+            ImageIO.write(imageS, "jpg", new File("img" + i + ".jpg"));
+        } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Error while writing to the image.");
             ex.printStackTrace();
         }
     }
-
 }
