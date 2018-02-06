b4415290d8eeb9f7d283a7a5433a96944307ca82
Kevin L. Rak <kevinlrak@users.noreply.github.com>
2018-02-06 13:25:45 -0500

    use NASA filename to build a large library of images over time; check if image already exists

--- Main.java
+++ Main.java
@@ -17,14 +17,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 
 public class Main
 {
-
     public static void main(String args[])
-    {
-        for (int i = 0; i < 10; i++)   // typically between 10 - 15 images can be downloaded
-    	    saveImage(parseXml(i), i); // Save the image pulled from NASA's RSS feed
-    }
-    
-    private static String parseXml(int i) // Returns the image link pulled from the RSS feed
     {
     	String imagelink = "";
         try 
@@ -35,31 +28,41 @@ public class Main
             Document doc=dBuilder.parse(conn.getInputStream());
             doc.getDocumentElement().normalize();
             NodeList nList=doc.getElementsByTagName("item");
-            Node nNode=nList.item(i);
-          
-            if(nNode.getNodeType() == Node.ELEMENT_NODE)
-            {
-                Element eElement=(Element) nNode;
-                String temp=eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("url")+"";
-                imagelink=temp.replace("url=\"", "").replace("\"", "").trim();
-            }
-            return imagelink;
+			
+			for (int i = 0; i < nList.getLength(); i++)
+			{
+				Node nNode=nList.item(i);
+			  
+				if(nNode.getNodeType() == Node.ELEMENT_NODE)
+				{
+					Element eElement=(Element) nNode;
+					String temp=eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("url")+"";
+					imagelink=temp.replace("url=\"", "").replace("\"", "").trim();
+				}
+				
+				saveImage(imagelink); // Save the image pulled from NASA's RSS feed
+			}
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Error while getting RSS.\nPerhaps try checking your internet connection?");
-            return null;
         }
     }
     
-    public static void saveImage(String imageURL, int i) // save the image to the specified location
+    public static void saveImage(String imageURL) // save the image to the specified location
     {
     	// For troubleshooting:
         System.out.println(imageURL.substring(0,4) + "s" + imageURL.substring(4));
         try {
             // Get the image from the URL, and set it to the image files
             URL url = new URL(imageURL.substring(0,4) + "s" + imageURL.substring(4));
-            BufferedImage imageS = null;
-            imageS = ImageIO.read(url);
-            ImageIO.write(imageS, "jpg", new File("img" + i + ".jpg"));
+			
+			String urlFileName = url.getFile();
+			String fileName = urlFileName.substring(urlFileName.lastIndexOf("/")+1, urlFileName.length());
+			File imageFile = new File(fileName);
+			if (!imageFile.exists())
+			{	// NOTE: this will eventually fill up the heap and crash after about 20 images
+				// java.lang.OutOfMemoryError: Java heap space
+				ImageIO.write(ImageIO.read(url), "jpg", imageFile);
+			}
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(null, "Error while writing to the image.");
             ex.printStackTrace();
